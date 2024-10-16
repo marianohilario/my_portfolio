d8393acce4065c83465fc7ccfddaf55873edc325
marianohilario <marianohilario@gmail.com>
2024-10-16 16:51:02 -0300

    added styles and languages

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const burguer = document.querySelector(".burguer");
 const navlist = document.querySelector(".navlist");
 const closeBtn = document.querySelector(".closeBtn");
+const links = document.querySelectorAll(".navbar__link");
+const resumeBtn = document.querySelector("#resumeBtn");
 
 burguer.addEventListener("click", () => {
     navlist.classList.add("visible");
@@ -10,65 +12,95 @@ closeBtn.addEventListener("click", () => {
     navlist.classList.remove("visible");
 });
 
-const contenedor = document.querySelector(".navbar__location");
+links.forEach((link) => {
+    link.addEventListener("click", () => {
+        navlist.classList.remove("visible");
+    });
+});
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-        (position) => {
-            const lat = position.coords.latitude;
-            const lon = position.coords.longitude;
+// const contenedor = document.querySelector(".navbar__location");
 
-            const ubicationDiv = document.createElement("h5");
-            const hourDiv = document.createElement("p");
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(
+//         (position) => {
+//             const lat = position.coords.latitude;
+//             const lon = position.coords.longitude;
 
-            ubicationDiv.id = "ubicacion";
-            contenedor.appendChild(ubicationDiv);
+//             const ubicationDiv = document.createElement("h5");
+//             const hourDiv = document.createElement("p");
 
-            fetch(
-                `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=es`
-            )
-                .then((response) => response.json())
-                .then((data) => {
-                    const ubication = `${data.countryName || "Desc."}, 
-                        ${data.city || "Desc."}, ${data.locality || "Desc."}`;
+//             ubicationDiv.id = "ubicacion";
+//             contenedor.appendChild(ubicationDiv);
 
-                    ubicationDiv.innerText = `${ubication}`;
-                });
+//             fetch(
+//                 `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=es`
+//             )
+//                 .then((response) => response.json())
+//                 .then((data) => {
+//                     const ubication = `${data.countryName || "Desc."},
+//                         ${data.city || "Desc."}, ${data.locality || "Desc."}`;
 
-            hourDiv.id = "hora";
-            contenedor.appendChild(hourDiv);
+//                     ubicationDiv.innerText = `${ubication}`;
+//                 });
 
-            function mostrarHora() {
-                const now = new Date();
-                hourDiv.innerText = `${now.toLocaleTimeString()}`;
-            }
+//             hourDiv.id = "hora";
+//             contenedor.appendChild(hourDiv);
 
-            setInterval(mostrarHora, 1000);
-            mostrarHora();
-        },
-        () => {
-            // If user doesn't allow geolocalization
-            console.warn("Failed to get user location.");
-        }
-    );
-} else {
-    console.warn("Geolocalization doesn't supported by this browser.");
-}
+//             function mostrarHora() {
+//                 const now = new Date();
+//                 hourDiv.innerText = `${now.toLocaleTimeString()}`;
+//             }
 
+//             setInterval(mostrarHora, 1000);
+//             mostrarHora();
+//         },
+//         () => {
+//             // If user doesn't allow geolocalization
+//             console.warn("Failed to get user location.");
+//         }
+//     );
+// } else {
+//     console.warn("Geolocalization doesn't supported by this browser.");
+// }
 
 // Inicializa EmailJS
 (function () {
     emailjs.init("6fzMTLSTwSp-QIVDY"); // Reemplaza con tu User ID
 })();
 
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita que la página se recargue
+document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+        event.preventDefault();
 
-    emailjs.sendForm('service_w1rqglb', 'template_uzwtfvp', this)
-        .then(function() {
-            alert('Mensaje enviado con éxito!');
-            document.getElementById('contact-form').reset(); // Reinicia el formulario
-        }, function(error) {
-            alert('Error al enviar el mensaje: ' + JSON.stringify(error));
-        });
+        emailjs.sendForm("service_w1rqglb", "template_uzwtfvp", this).then(
+            function () {
+                alert("Mensaje enviado con éxito!");
+                document.getElementById("contact-form").reset();
+            },
+            function (error) {
+                alert("Error al enviar el mensaje: " + JSON.stringify(error));
+            }
+        );
+    });
+
+const langBtns = document.querySelectorAll("[data-language]");
+
+const textToChange = document.querySelectorAll("[data-section]");
+
+langBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        resumeBtn.download = `MarianoHilarioResume - ${btn.dataset.language.toUpperCase()}.pdf`;
+        resumeBtn.href = `/assets/resumes/MarianoHilarioResume - ${btn.dataset.language.toUpperCase()}.pdf`;
+        fetch(`../languages/${btn.dataset.language}.json`)
+            .then((response) => response.json())
+            .then((data) => {
+                textToChange.forEach((element) => {
+                    const section = element.dataset.section;
+                    const value = element.dataset.value;
+
+                    element.innerHTML = data[section][value];
+                });
+            });
+    });
 });
