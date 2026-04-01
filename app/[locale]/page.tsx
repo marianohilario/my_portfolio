cff8ab20620febf89fef65f19e9e70502d293361
marianohilario <marianohilario@gmail.com>
2026-04-01 17:07:25 -0300

    feat: GoHome button created

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,15 +1,16 @@
-import Navbar from '@/components/Navbar'
-import SocialSidebar from '@/components/SocialSidebar'
-import Hero from '@/components/Hero'
-import About from '@/components/About'
-import Skills from '@/components/Skills'
-import Projects from '@/components/Projects'
-import Contact from '@/components/Contact'
-import Footer from '@/components/Footer'
-import { locales } from '@/lib/i18n'
+import Navbar from "@/components/Navbar";
+import SocialSidebar from "@/components/SocialSidebar";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+import { locales } from "@/lib/i18n";
+import GoHome from "@/components/GoHome";
 
 export function generateStaticParams() {
-  return locales.map((locale) => ({ locale }))
+  return locales.map((locale) => ({ locale }));
 }
 
 export default function Home() {
@@ -25,6 +26,7 @@ export default function Home() {
         <Contact />
       </main>
       <Footer />
+      <GoHome />
     </>
-  )
+  );
 }
---
+++ components/GoHome.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const GoHome = () => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShow(window.scrollY > 100);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <a
+      href="#home"
+      className={`fixed bottom-20 right-[5%] glass-card w-10 aspect-square rounded-full flex items-center justify-center cursor-pointer z-50 transition-transform! duration-300 ease-in-out hover:scale-110 ${show ? "opacity-100 scale-100" : "opacity-0 scale-75 pointer-events-none"}`}
+    >
+      <i className="ri-arrow-up-wide-line text-white text-3xl" />
+    </a>
+  );
+};
+
+export default GoHome;
