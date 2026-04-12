eb50c9b3c310bdd76f8a2b4eb0acc0a4aba09e79
marianohilario <marianohilario@gmail.com>
2026-04-12 16:00:31 -0300

    feat: automatic change color GoHome

--- components/GoHome.tsx
+++ components/GoHome.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { useTheme } from "next-themes";
 import React, { useEffect, useState } from "react";
 
 const GoHome = () => {
   const [show, setShow] = useState(false);
+  const { theme } = useTheme();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -19,7 +21,10 @@ const GoHome = () => {
       href="#home"
       className={`fixed bottom-20 right-[5%] glass-card w-10 aspect-square rounded-full flex items-center justify-center cursor-pointer z-50 transition-transform! duration-300 ease-in-out hover:scale-110 ${show ? "opacity-100 scale-100" : "opacity-0 scale-75 pointer-events-none"}`}
     >
-      <i className="ri-arrow-up-wide-line text-white text-3xl" />
+      <i
+        className="ri-arrow-up-wide-line text-3xl"
+        style={{ color: "var(--text-primary)" }}
+      />
     </a>
   );
 };
